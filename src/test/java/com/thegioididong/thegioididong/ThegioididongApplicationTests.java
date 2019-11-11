01dcbebbdc7e922af828c4b5a8be2ad6753d0ed6
nhok9xyeuem <nhuconghaui@gmail.com>
2019-11-11 12:12:20 +0700

    build gradlew

---
+++ src/test/java/com/thegioididong/thegioididong/ThegioididongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegioididong.thegioididong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThegioididongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
